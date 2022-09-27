e7fcd530b849ee883ddc4c5515bd9ad5869d059b
ludinhquan <ludinhquan@gmail.com>
2022-09-27 09:06:32 +0700

    m-auth: update auth module scope

--- libs/core/src/auth/auth.module.ts
+++ libs/core/src/auth/auth.module.ts
@@ -1,8 +1,9 @@
-import {Module} from "@nestjs/common";
+import {Global, Module} from "@nestjs/common";
 import {JwtModule} from "@nestjs/jwt";
 import {PassportModule} from "@nestjs/passport";
 import {JwtStrategy} from "./guard";
 
+@Global()
 @Module({
   imports: [
     PassportModule,
