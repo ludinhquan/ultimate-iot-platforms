73816d93756e23844e5e2d8b89d38b594c2751e3
ldquan <ludinhquan@gmail.com>
2022-10-02 22:31:31 +0700

    fix: update watched list

--- libs/core/src/domain/watched-list.ts
+++ libs/core/src/domain/watched-list.ts
@@ -1,96 +1,37 @@
-export abstract class WatchedList<T> {
+type Field = string | number
+type MapItems<T> = Map<Field, T>
 
-  public currentItems: T[];
-  private initial: T[];
-  private new: T[];
-  private removed: T[];
+export abstract class WatchedList<T> {
+  private items: MapItems<T>
 
   constructor (initialItems?: T[]) {
-    this.currentItems = initialItems ? initialItems : [];
-    this.initial = initialItems ? initialItems : [];
-    this.new = [];
-    this.removed = [];
+    this.items = this.toMap(initialItems ?? [])
   }
 
+  abstract getUniqueField(item: T): Field
   abstract compareItems (a: T, b: T): boolean;
 
+  private toMap(list: T[]){
+    return new Map(list.map(item => [item[this.getUniqueField(item)], item]))
+  }
+
+  private mapToArray(mapItems: MapItems<T>){
+    return [...mapItems].map(item => item[1])
+  }
+
   public getItems (): T[] {
-    return this.currentItems;
+    return this.mapToArray(this.items)
   }
 
-  public getNewItems (): T[] {
-    return this.new;
+  public add(item: T): void {
+    this.items.set(this.getUniqueField(item), item)
   }
 
-  public getRemovedItems (): T[] {
-    return this.removed;
-  }
-  
-  private isCurrentItem (item: T): boolean {
-    return this.currentItems
-      .filter((v: T) => this.compareItems(item, v)).length !== 0
+  public remove(item: T): void {
+    this.items.delete(this.getUniqueField(item))
   }
 
-  private isNewItem (item: T): boolean {
-    return this.new
-      .filter((v: T) => this.compareItems(item, v)).length !== 0
-  }
-
-  private isRemovedItem (item: T): boolean {
-    return this.removed
-      .filter((v: T) => this.compareItems(item, v))
-      .length !== 0
-  }
-
-  private removeFromNew (item: T): void {
-    this.new = this.new
-    .filter((v) => !this.compareItems(v, item));
-  }
-
-  private removeFromCurrent (item: T): void {
-    this.currentItems = this.currentItems
-        .filter((v) => !this.compareItems(item, v))
-  }
-
-  private removeFromRemoved (item: T): void {
-    this.removed = this.removed
-        .filter((v) => !this.compareItems(item, v))
-  }
-
-  private wasAddedInitially (item: T): boolean {
-    return this.initial
-      .filter((v: T) => this.compareItems(item, v))
-      .length !== 0
-  }
-
-  public exists (item: T): boolean {
-    return this.isCurrentItem(item);
-  }
-
-  public add (item: T): void {
-    if (this.isRemovedItem(item)) {
-      this.removeFromRemoved(item);
-    }
-
-    if (!this.isNewItem(item) && !this.wasAddedInitially(item)) {
-      this.new.push(item);
-    }
-
-    if (!this.isCurrentItem(item)) {
-      this.currentItems.push(item);
-    }
-  }
-
-  public remove (item: T): void {
-    this.removeFromCurrent(item);
-
-    if (this.isNewItem(item)) {
-      this.removeFromNew(item);
-      return;
-    }
-
-    if (!this.isRemovedItem(item)) {
-      this.removed.push(item);
-    }
+  public exists(field: Field): boolean {
+    return this.items.has(field)
   }
 }
