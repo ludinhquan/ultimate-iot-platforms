c7ae5d927bb0775d67794cb7923c89726e0bb1cb
ldquan <ludinhquan@gmail.com>
2022-10-06 22:50:03 +0700

    fix: naming repository

--- apps/service-datasource/src/data-access/interfaces/index.ts
+++ apps/service-datasource/src/data-access/interfaces/index.ts
@@ -1,6 +1,6 @@
-export * from './datasource-repository.interface'
-export * from './connection-repository.interface'
-export * from './device-repository.interface'
-export * from './system-device-repository.interface'
+export * from './datasource.repository.interface'
+export * from './connection.repository.interface'
+export * from './device.repository.interface'
+export * from './system-device.repository.interface'
 
 export * from './repository-manager.interface'
